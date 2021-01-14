1e451e5a97b191a00a60e55ede6f50d2b6cc03b9
zubair-haider <zubair.haider@gigalabs.co>
2021-01-14 14:12:05 +0500

    Code refactoring

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,155 +1,150 @@
 <?php
- namespace App\Controller;
+namespace App\Controller;
 
- use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
- use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
- use Symfony\Component\HttpFoundation\JsonResponse;
- use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
- class ApiController extends AbstractController
- {
+class ApiController extends AbstractController
+{
 
-  /**
-   * @var integer HTTP status code - 200 (OK) by default
-   */
-  protected $statusCode = 200;
+    /**
+     * @var integer HTTP status code - 200 (OK) by default
+     */
+    protected $statusCode = 200;
 
-  /**
-   * Gets the value of statusCode.
-   *
-   * @return integer
-   */
-  public function getStatusCode()
-  {
-   return $this->statusCode;
-  }
+    /**
+     * Gets the value of statusCode.
+     *
+     * @return integer
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
 
-  /**
-   * Sets the value of statusCode.
-   *
-   * @param integer $statusCode the status code
-   *
-   * @return self
-   */
-  protected function setStatusCode($statusCode)
-  {
-   $this->statusCode = $statusCode;
+    /**
+     * Sets the value of statusCode.
+     *
+     * @param integer $statusCode the status code
+     *
+     * @return self
+     */
+    protected function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
 
-   return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * Returns a JSON response
-   *
-   * @param array $data
-   * @param array $headers
-   *
-   * @return JsonResponse
-   */
-  public function response($data, $headers = [])
-  {
-   return new JsonResponse($data, $this->getStatusCode(), $headers);
-  }
+    /**
+     * Returns a JSON response
+     *
+     * @param array $data
+     * @param array $headers
+     *
+     * @return JsonResponse
+     */
+    public function response($data, $headers = [])
+    {
+        return new JsonResponse($data, $this->getStatusCode(), $headers);
+    }
 
-  /**
-   * Sets an error message and returns a JSON response
-   *
-   * @param string $errors
-   * @param $headers
-   * @return JsonResponse
-   */
-  public function respondWithErrors($errors, $headers = [])
-  {
-   $data = [
-    'status' => $this->getStatusCode(),
-    'errors' => $errors,
-   ];
+    /**
+     * Sets an error message and returns a JSON response
+     *
+     * @param string $errors
+     * @param $headers
+     * @return JsonResponse
+     */
+    public function respondWithErrors($errors, $headers = [])
+    {
+        $data = [
+            'status' => $this->getStatusCode(),
+            'errors' => $errors,
+        ];
 
-   return new JsonResponse($data, $this->getStatusCode(), $headers);
-  }
+        return new JsonResponse($data, $this->getStatusCode(), $headers);
+    }
 
+    /**
+     * Sets an error message and returns a JSON response
+     *
+     * @param string $success
+     * @param $headers
+     * @return JsonResponse
+     */
+    public function respondWithSuccess($success, $headers = [])
+    {
+        $data = [
+            'status' => $this->getStatusCode(),
+            'success' => $success,
+        ];
 
-  /**
-   * Sets an error message and returns a JSON response
-   *
-   * @param string $success
-   * @param $headers
-   * @return JsonResponse
-   */
-  public function respondWithSuccess($success, $headers = [])
-  {
-   $data = [
-    'status' => $this->getStatusCode(),
-    'success' => $success,
-   ];
+        return new JsonResponse($data, $this->getStatusCode(), $headers);
+    }
 
-   return new JsonResponse($data, $this->getStatusCode(), $headers);
-  }
+    /**
+     * Returns a 401 Unauthorized http response
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    public function respondUnauthorized($message = 'Not authorized!')
+    {
+        return $this->setStatusCode(401)->respondWithErrors($message);
+    }
 
+    /**
+     * Returns a 422 Unprocessable Entity
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    public function respondValidationError($message = 'Validation errors')
+    {
+        return $this->setStatusCode(422)->respondWithErrors($message);
+    }
 
-  /**
-   * Returns a 401 Unauthorized http response
-   *
-   * @param string $message
-   *
-   * @return JsonResponse
-   */
-  public function respondUnauthorized($message = 'Not authorized!')
-  {
-   return $this->setStatusCode(401)->respondWithErrors($message);
-  }
+    /**
+     * Returns a 404 Not Found
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    public function respondNotFound($message = 'Not found!')
+    {
+        return $this->setStatusCode(404)->respondWithErrors($message);
+    }
 
-  /**
-   * Returns a 422 Unprocessable Entity
-   *
-   * @param string $message
-   *
-   * @return JsonResponse
-   */
-  public function respondValidationError($message = 'Validation errors')
-  {
-   return $this->setStatusCode(422)->respondWithErrors($message);
-  }
+    /**
+     * Returns a 201 Created
+     *
+     * @param array $data
+     *
+     * @return JsonResponse
+     */
+    public function respondCreated($data = [])
+    {
+        return $this->setStatusCode(201)->response($data);
+    }
 
-  /**
-   * Returns a 404 Not Found
-   *
-   * @param string $message
-   *
-   * @return JsonResponse
-   */
-  public function respondNotFound($message = 'Not found!')
-  {
-   return $this->setStatusCode(404)->respondWithErrors($message);
-  }
+    // this method allows us to accept JSON payloads in POST requests
+    // since Symfony 4 doesn’t handle that automatically:
 
-  /**
-   * Returns a 201 Created
-   *
-   * @param array $data
-   *
-   * @return JsonResponse
-   */
-  public function respondCreated($data = [])
-  {
-   return $this->setStatusCode(201)->response($data);
-  }
+    protected function transformJsonBody(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
 
-  // this method allows us to accept JSON payloads in POST requests
-  // since Symfony 4 doesn’t handle that automatically:
+        if ($data === null) {
+            return $request;
+        }
 
-  protected function transformJsonBody(\Symfony\Component\HttpFoundation\Request $request)
-  {
-   $data = json_decode($request->getContent(), true);
+        $request->request->replace($data);
 
-   if ($data === null) {
-    return $request;
-   }
+        return $request;
+    }
 
-   $request->request->replace($data);
-
-   return $request;
-  }
-
-
- }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,49 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends ApiController
 {
-    private $userRepository;
-
-    public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
-
     public function login(Request $request)
     {
-        $user = $this->getUser();
-        return $this->json([
-            'username' => $user->getEmail(),
-            'roles' => $user->getRoles(),
-        ]);
+        try {
+            $user = $this->getUser();
+            $data = [
+                'username' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+            ];
+            return $this->respondWithSuccess($data);
+        } catch (\Exception $e) {
+            return $this->respondValidationError($e->getMessage());
+        }
     }
 
     public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        $em = $this->getDoctrine()->getManager();
-        $request = $this->transformJsonBody($request);
-        $password = $request->get('password');
-        $email = $request->get('email');
-        $roles = $request->get('roles');
-
-        if (empty($password) || empty($email) || empty($roles)) {
-            return $this->respondValidationError("Invalid Roles or Password or Email");
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $request = $this->transformJsonBody($request);
+            $password = $request->get('password');
+            $email = $request->get('email');
+            if (empty($password) || empty($email)) {
+                return $this->respondValidationError("Invalid Roles or Password or Email");
+            }
+            $user = new User($email);
+            $user->setPassword($encoder->encodePassword($user, $password));
+            $em->persist($user);
+            $em->flush();
+            return $this->respondWithSuccess(sprintf('User %s successfully created', 'a'));
+        } catch (\Exception $e) {
+            return $this->respondValidationError($e->getMessage());
         }
-
-        $user = new User($email);
-        $user->setPassword($encoder->encodePassword($user, $password));
-        $user->setEmail($email);
-        $user->setRoles($roles);
-        $em->persist($user);
-        $em->flush();
-        return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
     }
 }
